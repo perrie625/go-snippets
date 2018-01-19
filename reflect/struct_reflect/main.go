539394f6e379212baf8a1eada47e868c9726d4d7
QianPeili <qianperry@outlook.com>
2018-01-19 17:51:20 +0800

    Add set field use reflect

--- reflect/struct_reflect/main.go
+++ reflect/struct_reflect/main.go
@@ -11,10 +11,21 @@ var (
 	typeMap = make(map[string]reflect.Type)
 )
 
-type foo struct{}
+type foo struct {
+	Name    string
+	private string
+}
 
 func (f *foo) speak(word string) {
 	fmt.Println(word)
+}
+
+func (f *foo) sayName() {
+	if f.Name == "" {
+		fmt.Println("I have no name.")
+		return
+	}
+	fmt.Println(f.Name)
 }
 
 func registeType(object interface{}) {
@@ -32,7 +43,27 @@ func newObject(name string) (interface{}, error) {
 	return nil, errors.New("no this type")
 }
 
+func setAttr(object interface{}, field string, value string) bool {
+	objectValue := reflect.ValueOf(object)
+	if objectValue.Kind() != reflect.Ptr {
+		return false
+	}
+	objectValue = objectValue.Elem()
+	if !objectValue.CanSet() {
+		return false
+	}
+	fieldValue := objectValue.FieldByName(field)
+	fieldValue.SetString(value)
+	return true
+}
+
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+		}
+	}()
+
 	registeType(foo{})
 	obj, err := newObject("foo")
 	if err != nil {
@@ -42,4 +73,12 @@ func main() {
 	sample, ok := obj.(*foo)
 	fmt.Println(ok)
 	sample.speak("Hello, World.")
+
+	fmt.Printf("set successful ? %t\n", setAttr(*sample, "Name", "foo"))
+	sample.sayName()
+
+	fmt.Printf("set successful ? %t\n", setAttr(sample, "Name", "foo"))
+	sample.sayName()
+
+	setAttr(sample, "private", "hahaha")
 }
